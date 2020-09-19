d204d773d69eaadd431f616529775f0e196ee4cf
DeLuca M Thurmond <54365929+cdmvthurmonds727@users.noreply.github.com>
2020-09-18 23:05:12 -0500

    Update site.pp
    removed the information

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,2 @@
 node default {
-  file {'root/README.md':
-    ensure  => file,
-    content => 'This was an update',
-    owner   => 'root',
-  }  
-  file {'root/README.md':
-    owner => 'root',
-  }
 }
