2bb7be69eebbd6dc049417a86fa475e6bf4cf328
DeLuca M Thurmond <54365929+cdmvthurmonds727@users.noreply.github.com>
2020-09-21 13:06:59 -0500

    Update site.pp
    Added more roles to the site.pp file

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,18 @@
 node default {
-
+    file { 'root/README':
+      ensure  => file,
+      contect => 'This is a readme',
+      owner   => 'root',
+    }  
 }
 node 'localhost.localdomain' {
   include role::master_server
+}
+
+node /^web/ {
+  include role::app_server
+}
+
+node /^db/ {
+  include role::db_server
 }
