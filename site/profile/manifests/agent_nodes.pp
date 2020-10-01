39b3f668a60feea198fe2c51b81088d74eeb03a2
DeLuca M Thurmond <54365929+cdmvthurmonds727@users.noreply.github.com>
2020-10-01 11:09:07 -0500

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,6 +1,6 @@
 class profiles::agent_nodes {
   include dockeragent
-  dockeragent::node {'localhost'}
+  dockeragent::node {'localhost':}
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
 }
