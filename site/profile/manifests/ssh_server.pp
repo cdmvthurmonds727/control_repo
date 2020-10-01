5378652b6fd0a93d458e32c5eaace636999fd513
DeLuca M Thurmond <54365929+cdmvthurmonds727@users.noreply.github.com>
2020-10-01 17:56:53 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
               ensure => present.
               user   => 'root',
               type   => 'ssh-rsa',
-              key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCq4vks2GYPoj6kfvX1ZEtlgxygkfetwXyeDTKigewpdDsl42nMra8Q4TFe4O9lNFL0Au1OlZzAYCoz8qbvIobKgshb4Je6c+o1X5uyUn0KJJIzzSjWgpyWiMzRfkVxkSg1UdMySQMGJ7C41o93pD3TCFPdzQNZCpopWPmZ/QVq8kDP99Tc+yj6Figv9KVfJ2c6aM682JvTTu2g6mDHdqfnv6/BO5P/DFHFd0ZHKNgmxTyGycjs9XYcpaMDRJfpJGRRZ+9YMhL1EZ0fsonUo7da8bUK4Lp2yXZ5CAVAvnBl7Q10U5Zltb/rv6iXUXIS2mCblZDh0xHNdxKOCl49Whnf',
+              key    => ' ',
 
 }
